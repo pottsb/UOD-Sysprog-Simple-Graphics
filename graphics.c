9235e8c4d1f6ad004f7e988d3430c3696c889050
Peter Bedford <peter.bedford2@gmail.com>
2023-12-01 23:17:28 +0000

    stage 1 done

--- graphics.c
+++ graphics.c
@@ -3,7 +3,31 @@
 #include "x86.h"
 #include "memlayout.h"
 
-int sys_setpixel(int hdc, int x, int y){
+
+struct point {
+    int x;
+    int y;
+};
+struct point mypoint= { .x = 1, .y = 0 };
+
+
+int sys_setpixel(void){
+
+    int hdc;
+    int x;
+    int y;
+
+    if (argint(0, &hdc) < 0) {
+        return -1;
+    }
+
+    if (argint(1, &x) < 0) {
+        return -1;
+    }
+
+    if (argint(2, &y) < 0) {
+        return -1;
+    }
 
     // Check if the coordinates are within the screen boundaries
     if (x < 0 || x >= 320 || y < 0 || y >= 200) {
@@ -11,21 +35,111 @@ int sys_setpixel(int hdc, int x, int y){
     }
     ushort offset = 320 * y + x;
 
-    //static ushort* crt = (ushort*)P2V(0xA0000); 
-    //crt[offset] = 0x0f;
-    //////////// the above code doesn't work. /////////////
-
-    // Calculate the memory address for the pixel in video memory
     char* videoMemory = (char*)P2V(0xA0000);
     videoMemory[offset] = 0x0f;
 
     return 0; // Return 0 to indicate success
 }
-int sys_moveto(int hdc, int x, int y){
-	return 0;
+int sys_moveto(void){
+
+    int hdc;
+    int x;
+    int y;
+
+    if (argint(0, &hdc) < 0) {
+        return -1;
+    }
+
+    if (argint(1, &x) < 0) {
+        return -1;
+    }
+
+    if (argint(2, &y) < 0) {
+        return -1;
+    }
+
+    // Check if the x-coordinate is out of bounds
+    if (x < 0) {
+        x = 0;
+    } else if (x > 320) {
+        x = 320;
+    }
+
+    // Check if the y-coordinate is out of bounds
+    if (y < 0) {
+        y = 0;
+    } else if (y > 200) {
+        y = 200;
+    }
+
+
+	mypoint.x = x;
+    mypoint.y = y;
+
+    return 1;
 }
-int sys_lineto(int hdc, int x, int y){
-	return 0;
+
+int abs(int n) {
+    return (n < 0) ? -n : n;
+}
+
+int sys_lineto(void){
+	int hdc;
+    int x2;
+    int y2;
+
+    if (argint(0, &hdc) < 0) {
+        return -1;
+    }
+
+    if (argint(1, &x2) < 0) {
+        return -1;
+    }
+
+    if (argint(2, &y2) < 0) {
+        return -1;
+    }
+
+    // Check if the coordinates are within the screen boundaries
+    if (x2 < 0 || x2 >= 320 || y2 < 0 || y2 >= 200) {
+        return -1; // Return an error code to indicate out-of-bounds
+    }
+
+    int y1 = mypoint.y;
+    int x1 = mypoint.x;
+
+
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+
+    while (1) {
+        ushort offset = 320 * y1 + x1;
+        char* videoMemory = (char*)P2V(0xA0000);
+        videoMemory[offset] = 0x0f;
+
+        if (x1 == x2 && y1 == y2) {
+            break;
+        }
+
+        if (err > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if (err < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+
+    mypoint.x = x2;
+    mypoint.y = y2;
+
+    return 1;
+
 }
 
 void clear320x200x256() {
--- test.c
+++ test.c
@@ -4,11 +4,19 @@
 int main(int argc, char* argv[])
 {
     setvideomode(0x13);
+
+    setpixel(1,10,10);
+    setpixel(1,11,10);
+    setpixel(1,10,11);
+    setpixel(1,11,11);
+
     moveto(0, 100, 50);
     lineto(0, 200, 50);
     lineto(0, 200, 150);
     lineto(0, 100, 150);
     lineto(0, 100, 50);
+
+
     getch();
     setvideomode(0x03);
     exit();
